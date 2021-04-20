7b108e740ef1e5e612d4104fc563ff19b3b7347d
felix <felix01.schneider@gmail.com>
2021-04-20 17:55:46 +0200

    Automatically login known user

--- app/src/main/java/com/plandora/activity/launch/SplashScreenActivity.kt
+++ app/src/main/java/com/plandora/activity/launch/SplashScreenActivity.kt
@@ -4,26 +4,61 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.plandora.R
+import com.plandora.activity.main.MainActivity
+import com.plandora.controllers.PlandoraEventController
+import com.plandora.controllers.PlandoraUserController
+import com.plandora.crud_workflows.CRUDActivity
 
-class SplashScreenActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity(), CRUDActivity {
+
+    companion object {
+        private const val SPLASH_SCREEN_DELAY: Long = 2000
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        
-        // hide all screen decoration (status bar etc.)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-
-        // start SignInActivity after a short delay
-        Handler().postDelayed({
-            startActivity(Intent(this, SignInActivity::class.java))
-            finish()
-        }, 2000)
+        hideScreenDecoration()
+        resumeAfterDelay()
     }
+
+    private fun hideScreenDecoration() {
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+    }
+
+    private fun resumeAfterDelay() {
+        Handler().postDelayed({
+            resumeAfterSplashScreen()
+        }, SPLASH_SCREEN_DELAY)
+    }
+
+    private fun resumeAfterSplashScreen() {
+        startActivityAfterSplashScreen()
+        finish()
+    }
+
+    private fun startActivityAfterSplashScreen() {
+        if(PlandoraUserController().currentUserId().isNotEmpty()) {
+            setUpMainActivity()
+            return
+        }
+        startActivity(Intent(this, SignInActivity::class.java))
+    }
+
+    private fun setUpMainActivity() {
+        PlandoraEventController().getEventList(this)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    override fun onInternalFailure(message: String) {
+        Toast.makeText(this, "An internal error occured", Toast.LENGTH_LONG).show()
+    }
+
 
 }
