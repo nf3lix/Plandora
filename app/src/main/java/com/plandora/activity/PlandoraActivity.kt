aa7d82132c4a39e19440f6b0ebac801f6cb0ff76
nf3lix <felix01.schneider@gmail.com>
2021-05-22 18:55:58 +0200

    Add progress bar dialog to base activity

--- app/src/main/java/com/plandora/activity/PlandoraActivity.kt
+++ app/src/main/java/com/plandora/activity/PlandoraActivity.kt
@@ -1,12 +1,29 @@
 package com.plandora.activity
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
+import com.plandora.R
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.dialog_progress.*
+import kotlinx.android.synthetic.main.dialog_progress.view.*
 
 open class PlandoraActivity : AppCompatActivity() {
 
+    private lateinit var progressBar: Dialog
+
     open fun addActionBar() {
         setSupportActionBar(toolbar_main_activity)
+    }
+
+    fun showProgressBar(text: String = "Loading...") {
+        progressBar = Dialog(this)
+        progressBar.setContentView(R.layout.dialog_progress)
+        progressBar.progress_text.text = text
+        progressBar.show()
+    }
+
+    fun hideProgressBar() {
+        progressBar.dismiss()
     }
 
 }
