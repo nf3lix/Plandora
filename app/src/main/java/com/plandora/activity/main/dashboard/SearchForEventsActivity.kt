003919f9e3981e923fd6cd4e3df3fccfd1ac4e46
henry <henry-schulz@hotmail.de>
2021-05-23 12:46:23 +0200

    Create Layout for activity_search_for_events.xml

--- app/src/main/java/com/plandora/activity/main/dashboard/SearchForEventsActivity.kt
+++ app/src/main/java/com/plandora/activity/main/dashboard/SearchForEventsActivity.kt
@@ -2,15 +2,26 @@ package com.plandora.activity.main.dashboard
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import com.plandora.R
 import com.plandora.activity.PlandoraActivity
+import com.plandora.models.events.EventType
+import kotlinx.android.synthetic.main.activity_create_event.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class SearchForEventsActivity: PlandoraActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_search_for_events)
+        setContentView(R.layout.activity_search_for_events)
+        //event_type_spinner.adapter = ArrayAdapter<EventType>(this, R.layout.support_simple_spinner_dropdown_item, EventType.values())
+        event_type_spinner.adapter = ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, arrayListOf("Hallo", "Test"))
         addActionBar()
 
+    }
+
+    override fun addActionBar() {
+        setSupportActionBar(toolbar_main_activity)
     }
 
 
